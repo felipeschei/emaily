ea0476721895ba87120b190a254613f27647a817
felipe <felipescheidemantel@gmail.com>
2019-04-19 15:36:57 -0300

    cookie session set up

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '292695256369-2615naed0h2hhu2crrvd06r2musejhgl.apps.googleusercontent.com',
+    googleClientSecret: 'PVOOu-uPRzIMXpulsG7WInPV',
+    mongoURI: 'mongodb://felidev:emailydev1@ds147073.mlab.com:47073/emaily-dev',
+    cookieKey: 'asuhfasofjasfi)-8!@fasvkasn97a97asf79nAGUDADbbvas'
+};
